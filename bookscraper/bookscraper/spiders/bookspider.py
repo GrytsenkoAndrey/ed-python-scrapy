4bf0f134a7e79c3be80231eadc04c26d6446e350
Andrey Grytsenko <andreyapg@gmail.com>
2024-12-16 13:41:26 +0200

    scrape all pages 2024-12-16

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -21,3 +21,12 @@ class BooksSpider(scrapy.Spider):
                 'price': book.css('.product_price .price_color::text').get(),
                 'url': book.css('h3 a').attrib['href']
             }
+
+        next_page = response.css('li.next a ::attr(href)').get()
+
+        if next_page is not None:
+            if 'catalogue/' in next_page:
+                next_page_url = self.start_urls[0] + next_page
+            else:
+                next_page_url = self.start_urls[0] + 'catalogue/' + next_page
+            yield response.follow(next_page_url, callback=self.parse)
